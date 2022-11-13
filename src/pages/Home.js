661d91147ae26c42cc82dc92f3b68470bff5eb47
ajadimayowa <samtalktech@outlook.com>
2022-11-13 04:12:35 -0800

    begn desktop mode and handled routing

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import './home.css'
+
+function Home() {
+    return (
+        <div className='container'>
+            <left-section>
+
+            </left-section>
+
+            <right-section>
+
+            </right-section>
+
+        </div>
+    )
+}
+
+export default Home;
